21a562c8559d4ec9eea4de36abc7b7af91b67da1
Ivan Hueso <ivanhueso@gmail.com>
2019-01-28 19:31:28 -0300

    Lot of Work Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 import banner from '../images/selectcars.gif' // Tell Webpack this JS file uses this image
+import virginia from '../images/virginia_mp.png' // Tell Webpack this JS file uses this image
 
 export default class Index extends React.Component {
   getCurrentDate() {
@@ -41,38 +42,63 @@ export default class Index extends React.Component {
               Do not pay your next car insurance bill until you read this...
             </h3>
             <p class="date-text">{this.getCurrentDate()}</p>
-
-            <p>Welcome to your new Gatsby site. </p>
             <p>
-              Now go build something great.Now go build something great.Now go
-              build something great.Now go build something great.Now go build
-              something great.Now go build something great.Now go build
-              something great.Now go build something great.Now go build
-              something great.Now go build something great.Now go build
-              something great.Now go build something great.Now go build
-              something great.Now go build something great.
-              <br /> Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something great.
-              <br /> Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something great.
-              <br /> Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something
-              great.Now go build something great.Now go build something great.
+              <img
+                src={virginia}
+                style={{ float: `right`, marginLeft: '20px' }}
+                alt="Virginia Photo"
+                width="308"
+                height="185"
+              />
+              <strong>Seattle, Washington</strong>— New policies are indicating
+              that for years many Washington drivers have likely overpaid on
+              their car insurance.
               <br />
+              <br />
+              The simple truth is your car insurance company doesn't want you to
+              know this.
+              <br />
+              <br />
+              Did you know,{' '}
+              <strong>
+                if you drive less than 50 miles/day and live in a{' '}
+                <a href="#">qualified zip code</a>
+              </strong>{' '}
+              you can get an extremely high discount. Additionally, depending on
+              your age you can get even higher discounts. Has your insurance
+              agent ever told you that?
+            </p>
+            <p>
+              The problem is that insurance agents are paid on commission and
+              want to sell you the highest possible rate to increase their
+              paycheck. To fight back, many drivers have turned to BRAND NEW &
+              super popular <strong>"IAIS Technology"</strong> services to
+              discover{' '}
+              <strong>
+                MASSIVE discounts and policies as low as $29/month.
+              </strong>
+            </p>
+            <p>
+              Making a smart health insurance choice can save you hundreds or
+              even thousands in a year. We are experts in finding affordable
+              health insurance plans that meet your needs. These affordable
+              medical Insurance plans have a wide range of deductibles and low
+              cost premiums to fit your lifestyle needs and budget. Affordable
+              health Insurance quotes don’t have to be complicated. We make it
+              simple.
+            </p>
+            <h2>Here's How You Do It:</h2>
+            <p>
+              <strong>Step 1:</strong>{' '}
+              <a href="#">
+                Tap your state to instantly check your zip code for free
+              </a>
+            </p>
+            <p>
+              <strong>Step 2:</strong>
+              Once you go through a few questions, you will have the opportunity
+              to compare the best carrier quotes in your area for an average
+              savings of 32% a month.
             </p>
             <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
               <Image />
